5c021ca350baec180e5ea6f89d5851b62cdb6c11
dhruvinparikh <imsilent93@gmail.com>
2021-03-16 21:53:15 -0400

    add black list example

--- class-6/examples/fallback.sol
+++ class-6/examples/fallback.sol
@@ -4,15 +4,23 @@ pragma solidity ^0.7.6;
 contract Fallback {
     event Log(uint gas);
 
+    mapping(address => bool) public blacklist;
+
     // Fallback function must be declared as external.
     fallback() external payable {
         // send / transfer (forwards 2300 gas to this fallback function)
         // call (forwards all of the gas)
+        blacklist[msg.sender] = true;
         emit Log(gasleft());
     }
 
+    modifier onlyWhiteListed() {
+        require(!blacklist[msg.sender], "no no");
+        _;
+    }
+
     // Helper function to check the balance of this contract
-    function getBalance() public view returns (uint) {
+    function getBalance() public view onlyWhiteListed returns (uint) {
         return address(this).balance;
     }
 }
